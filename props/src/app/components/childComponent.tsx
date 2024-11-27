404087586a2b7b1c0d78d7e5bcbd463ae5dedee4
Ahsan Paracha <ahsanirfanparacha@gmail.com>
2024-11-27 23:37:42 +0500

    Class 11 Assignment

---
+++ props/src/app/components/childComponent.tsx
@@ -0,0 +1,10 @@
+//Child Component
+export default function Property(props: any){
+    return(
+        <div>
+        <h1>Hello my name is, {props.myName}</h1>
+        <h1>My favorite Wrestler is, {props.favWrestler}</h1>
+        <h1>My favorite color is, {props.favColor}</h1>
+        </div>
+    )
+}
---
+++ props/src/app/page.tsx
@@ -0,0 +1,12 @@
+//Parent Component
+import Property from "./components/childComponent";
+
+export default function Home() {
+  let myName: string = "Ahsan Paracha";
+  let favWrestler: string = "Roman Reigns";
+  let favColor: string = "Black"
+
+  return (
+    <Property myName={myName} favWrestler={favWrestler} favColor={favColor}/>
+  );
+}
